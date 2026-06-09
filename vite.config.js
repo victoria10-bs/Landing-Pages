0f11b089333d3bc4e306a36a165f0f246455f591
vik <victoriaschneider@gmail.com>
2026-06-09 17:46:10 -0300

    listo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  return {
   plugins: [react()],
-  base: '/Landing-Pages/'
+  base: command ==='build' ? '/Landing-Pages/' : '/'
+  }
 })
